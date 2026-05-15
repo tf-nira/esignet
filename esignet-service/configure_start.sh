7140c0ca37715ed431835110c15939fdf8c947c0
sjkarthik <58928645+sjkarthik@users.noreply.github.com>
2026-05-15 12:33:56 +0530

    Update configure_start.sh
    Signed-off-by: sjkarthik <58928645+sjkarthik@users.noreply.github.com>

--- esignet-service/configure_start.sh
+++ esignet-service/configure_start.sh
@@ -13,7 +13,7 @@ fi
 #installs the pkcs11 libraries.
 set -e
 
-DEFAULT_ZIP_PATH=artifactory/libs-release-local/hsm/ida_client.zip
+DEFAULT_ZIP_PATH=artifactory/libs-release-local/hsm/client.zip
 [ -z "$hsm_zip_file_path" ] && zip_path="$DEFAULT_ZIP_PATH" || zip_path="$hsm_zip_file_path"
 
 echo "Download the client from $artifactory_url_env"
